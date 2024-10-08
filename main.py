0e166e28ee8e30e624f94b18c01be870061c91e9
Jordannll <jorlaw4753@gmail.com>
2024-10-08 16:24:12 -0400

    Game and screen initialized

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # main
+import pygame
+
+# initializing game start
+pygame.init()
+clock = pygame.time.Clock()
+
+# initializing screen
+screen_width = 800
+screen_height = 600
+screen = pygae.display.set_mode((screen_wdith, screen_height))
