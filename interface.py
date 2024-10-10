e2e2aabe1c8368a3e7a8874540542f689660ee19
Zi Qing Xiong <ziqingxiong@Zis-MacBook-Pro.local>
2024-10-10 15:40:46 -0400

    imported player images

--- interface.py
+++ interface.py
@@ -57,22 +57,141 @@ def intro_clicks() -> int:
     # settings button
     if mouse_tact(740, 40, 75, 40):
         return mouse_click(740, 40, 75, 40, SETTINGS)
+    return mode
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# settings ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def settings():
-    screen.fill((0, 0, 255))
+    # background colour - dim purple
+    screen.fill((75, 76, 117))
 
+    # title text
+    pygame.font.init()
+    path = os.path.join(os.getcwd(), 'Assets', 'TitleFont.ttf')
+    title_font = pygame.font.Font(path, 70)
+    title = title_font.render("Settings", False, (237, 238, 255))
+    screen.blit(title, (80, 70))
+
+    volume_display = pygame.Rect(90, 185, 600, 120)
+    pygame.draw.rect(screen, (165, 167, 199), volume_display, 0, 20)
+
+    sfx_display = pygame.Rect(90, 335, 600, 120)
+    pygame.draw.rect(screen, (165, 167, 199), sfx_display, 0, 20)
+
+    # back button
+    back_button = Button(80, 40, 90, 40, 10, (165, 167, 199), 'home')
+    back_button.create_button()
+
+def settings_clicks() -> int:
+    # back button
+    if mouse_tact(80, 40, 90, 40):
+        return mouse_click(80, 40, 90, 40, INTRO)
+    return mode
+
+# instructions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def instructions():
-    screen.fill((255, 255, 255))
+    # background colour - dim purple
+    screen.fill((75, 76, 117))
 
+    # title text
+    pygame.font.init()
+    path = os.path.join(os.getcwd(), 'Assets', 'TitleFont.ttf')
+    title_font = pygame.font.Font(path, 70)
+    title = title_font.render("Story", False, (237, 238, 255))
+    screen.blit(title, (80, 70))
+
+    # main display
+    main_display = pygame.Rect(90, 150, 600, 330)
+    pygame.draw.rect(screen, (165, 167, 199), main_display, 0, 20)
+
+    # back button
+    back_button = Button(80, 40, 90, 40, 10, (165, 167, 199), 'home')
+    back_button.create_button()
+
+    # settings button
+    settings_button = Button(740, 40, 75, 40, 10, (171, 174, 222), 'set')
+    settings_button.create_button()
+
+def instructions_clicks():
+    # back button
+    if mouse_tact(80, 40, 90, 40):
+        return mouse_click(80, 40, 90, 40, INTRO)
+    # settings button
+    if mouse_tact(740, 40, 75, 40):
+        return mouse_click(740, 40, 75, 40, SETTINGS)
+    return mode
+
+# customizations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def customizations():
-    screen.fill((0, 255, 0))
+    # background colour - dim purple
+    screen.fill((75, 76, 117))
+    
+    # title text
+    pygame.font.init()
+    path = os.path.join(os.getcwd(), 'Assets', 'TitleFont.ttf')
+    title_font = pygame.font.Font(path, 70)
+    title = title_font.render("Customization", False, (237, 238, 255))
+    screen.blit(title, (80, 70))
 
+    # P1 display
+    p1_display = pygame.Rect(90, 150, 280, 350)
+    pygame.draw.rect(screen, (165, 167, 199), p1_display, 0, 20)
+    
+    # P1 text
+    title_font = pygame.font.Font(path, 40)
+    title = title_font.render("Player 1", False, (0, 0, 0))
+    screen.blit(title, (140, 440))
+
+    # next button
+    next_button = Button(330, 180, 50, 40, 10, (147, 149, 186), '>')
+    next_button.create_button()
+
+    # back button
+    back_button = Button(130, 180, 50, 40, 10, (147, 149, 186), '<')
+    back_button.create_button()
+
+    # ~~~~~~~~~~~~~~~~~~~
+
+    # P2 display
+    p2_display = pygame.Rect(420, 150, 280, 350)
+    pygame.draw.rect(screen, (165, 167, 199), p2_display, 0, 20)
+
+    # P1 text
+    title_font = pygame.font.Font(path, 40)
+    title = title_font.render("Player 2", False, (0, 0, 0))
+    screen.blit(title, (470, 440))
+
+    # next button
+    next_button = Button(660, 180, 50, 40, 10, (147, 149, 186), '>')
+    next_button.create_button()
+
+    # back button
+    back_button = Button(460, 180, 50, 40, 10, (147, 149, 186), '<')
+    back_button.create_button()
+
+    # back button
+    back_button = Button(80, 40, 90, 40, 10, (165, 167, 199), 'home')
+    back_button.create_button()
+
+    # settings button
+    settings_button = Button(740, 40, 75, 40, 10, (171, 174, 222), 'set')
+    settings_button.create_button()
+
+def customizations_clicks():
+    # back button
+    if mouse_tact(80, 40, 90, 40):
+        return mouse_click(80, 40, 90, 40, INTRO)
+    # settings button
+    if mouse_tact(740, 40, 75, 40):
+        return mouse_click(740, 40, 75, 40, SETTINGS)
+    return mode
+
+# game ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def game():
     # background colour - dim purple
     screen.fill((75, 76, 117))
 
+# gameover ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def gameover():
     pass
 
@@ -119,9 +238,8 @@ def mouse_tact(x, y, w, h) -> bool:
 
 def mouse_click(x, y, w, h, newMode) -> int:
     if (x - w/2 <= mouse[0] <= x + w/2) and (y - h/2 <= mouse[1] <= y + h/2):
-        if newMode == 707:
-            pygame.quit()
         return newMode
+    return mode
 
 mode = 0
 INTRO = 1
@@ -131,7 +249,7 @@ CUSTOMIZATIONS = 4
 GAME = 5
 GAMEOVER = 6
 
-mode = INTRO
+mode = CUSTOMIZATIONS
 
 # initializing game start
 pygame.init()
@@ -161,12 +279,20 @@ while running:
             intro()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mode = intro_clicks()
+                if mode == 707:
+                    running = False
         elif mode == SETTINGS:
             settings()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mode = settings_clicks()
         elif mode == INSTRUCTIONS:
             instructions()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mode = instructions_clicks()
         elif mode == CUSTOMIZATIONS:
             customizations()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mode = customizations_clicks()
         elif mode == GAME:
             game()
         elif mode == GAMEOVER:
